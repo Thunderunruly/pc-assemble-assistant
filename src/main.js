d60efa29a0db754bf55af0ea08c9cadddde73217
Thunderunruly <69342090+Thunderunruly@users.noreply.github.com>
2024-09-22 19:32:31 +1000

    Update basic frameworks

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import { create, NButton, NCard, NConfigProvider, NGlobalStyle, NSelect } from 'naive-ui'
+import router from './routes';
+import i18n from './i18n';
 
-createApp(App).mount('#app')
+const naive = create({
+    components: [NConfigProvider,NButton,NCard, NSelect, NGlobalStyle]
+});
+
+
+const app = createApp(App);
+app.use(i18n);
+app.use(router);
+app.use(naive);
+
+router.beforeEach( async ( to, from, next ) => {
+    const titleKey = "title." + (to.meta.title || "default");
+    document.title = i18n.global.t(titleKey);
+    next();
+});
+
+app.mount('#app');
